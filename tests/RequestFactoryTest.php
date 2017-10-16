723e3da211262dae5778d886c3ea80e85f23699d
Zhuk Sergey <seregazhuk88@gmail.com>
2017-10-16 20:54:38 +0300

    adds request factory tests

---
+++ tests/RequestFactoryTest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace seregazhuk\React\Memcached\tests;
+
+use PHPUnit\Framework\TestCase;
+use seregazhuk\React\Memcached\Protocol\Request\Factory;
+use seregazhuk\React\Memcached\Protocol\Request\Request;
+use seregazhuk\React\Memcached\Protocol\Request\SimpleRequest;
+use seregazhuk\React\Memcached\Protocol\Request\StorageRequest;
+
+class RequestFactoryTest extends TestCase
+{
+    /** @test */
+    public function it_returns_instance_of_request()
+    {
+        $factory = new Factory();
+        $this->assertInstanceOf(Request::class, $factory->create('version', []));
+    }
+
+    /** @test */
+    public function it_creates_simple_request_for_non_storage_commands()
+    {
+        $factory = new Factory();
+        $this->assertInstanceOf(SimpleRequest::class, $factory->create('version', []));
+    }
+
+    /** @test */
+    public function it_creates_storage_request_for_storage_commands()
+    {
+        $factory = new Factory();
+        $this->assertInstanceOf(StorageRequest::class, $factory->create('set', ['key', 'val']));
+    }
+}
