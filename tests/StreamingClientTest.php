e1e67c851b2e1773f77e24dd51a5d7ba1f4444c5
Zhuk Sergey <seregazhuk88@gmail.com>
2017-10-16 20:48:13 +0300

    adds tests for client

--- tests/StreamingClientTest.php
+++ tests/StreamingClientTest.php
@@ -6,6 +6,7 @@ use Mockery;
 use Mockery\MockInterface;
 use React\Stream\DuplexStreamInterface;
 use seregazhuk\React\Memcached\Client;
+use seregazhuk\React\Memcached\Exception\Exception;
 use seregazhuk\React\Memcached\Protocol\Parser;
 use seregazhuk\React\Memcached\Protocol\Request\Factory as RequestFactory;
 use seregazhuk\React\Memcached\Protocol\Response\Factory as ResponseFactory;
@@ -54,5 +55,24 @@ class StreamingClientTest extends TestCase
         $this->client->resolveRequests(['not_valid']);
 
         $this->expectPromiseRejects($promise);
+    }
+
+    /** @test */
+    public function it_resolves_a_promise_with_data_from_response()
+    {
+        $this->parser->shouldReceive('makeRequest')->andReturn("version\n\r");
+        $this->stream->shouldReceive('write')->once();
+        $promise = $this->client->version();
+
+        $this->client->resolveRequests(['version']);
+
+        $this->expectPromiseResolves($promise);
+    }
+
+    /** @test */
+    public function it_throws_exception_when_handling_response_data_without_pending_requests()
+    {
+        $this->expectException(Exception::class);
+        $this->client->resolveRequests(['not_valid']);
     }
 }
